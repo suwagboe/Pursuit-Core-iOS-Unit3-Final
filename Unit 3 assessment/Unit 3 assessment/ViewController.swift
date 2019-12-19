18920119b99fa12a6de558fdd41dd87cdc89c46c
uwagboe <sm.uwagboe@gmail.com>
2019-12-19 10:22:13 -0500

    practice push ...

---
+++ Unit 3 assessment/Unit 3 assessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unit 3 assessment
+//
+//  Created by Pursuit on 12/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Unit 3 assessment/Unit 3 assessmentTests/Unit_3_assessmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  Unit_3_assessmentTests.swift
+//  Unit 3 assessmentTests
+//
+//  Created by Pursuit on 12/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import XCTest
+@testable import Unit_3_assessment
+
+class Unit_3_assessmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
