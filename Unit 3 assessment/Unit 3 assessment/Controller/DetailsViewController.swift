e4940f7a8f77f230c98d50ffa124c65393be9a75
uwagboe <sm.uwagboe@gmail.com>
2019-12-19 16:25:33 -0500

    everything in details controller is now showing correctly working on  tableView image

--- Unit 3 assessment/Unit 3 assessment/Controller/DetailsViewController.swift
+++ Unit 3 assessment/Unit 3 assessment/Controller/DetailsViewController.swift
@@ -36,7 +36,7 @@ class DetailsViewController: UIViewController {
         
         elementNameLabel.text = "This elements name is \(e.name)"
         elementNumLabel.text = "The number associates with \(e.name) is \(e.number.description)"
-        elementSymbolLabel.text = " \(e.name) will melt at \(e.symbol) degrees"
+        elementSymbolLabel.text = " \(e.name) symbol is \(e.symbol)."
         weightLabel.text = "The weight of this element is \(e.density!)"
         discoveredbyLabel.text = " It was \(e.discovered_by!) that discovered this element"
       summaryLabel.text = e.summary
