6813c8b4d1196de4de2b339028ca3884df4fefc5
uwagboe <sm.uwagboe@gmail.com>
2019-12-19 11:18:16 -0500

    started the custom cell

---
+++ Unit 3 assessment/Unit 3 assessment/Views/ElementCell.swift
@@ -0,0 +1,30 @@
+//
+//  ElementCell.swift
+//  Unit 3 assessment
+//
+//  Created by Pursuit on 12/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ElementCell: UITableViewCell {
+    
+    @IBOutlet weak var elementNameLabel: UILabel!
+    @IBOutlet weak var elementSymbol: UILabel!// the number
+    @IBOutlet weak var elementThumbnailImage: UIImage!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configureCell( for element: Elements) {
+        
+     //   elementSymbol.text = element // need to get the symbol or the number for this part
+        elementNameLabel.text = element.name
+        //elementThumbnailImage.getImage
+        
+    }
+
+}
