2009a455d86e56b43f761dbefce07db61bc0a5ec
uwagboe <sm.uwagboe@gmail.com>
2019-12-19 10:39:30 -0500

    made all the folders and made the network services files: network helper, appError, and am currently working on APIClient

---
+++ Unit 3 assessment/Unit 3 assessment/Network Services/ElementsAPIClient.swift
@@ -0,0 +1,15 @@
+//
+//  ElementsAPIClient.swift
+//  Unit 3 assessment
+//
+//  Created by Pursuit on 12/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+struct ElementsSAPIClient {
+    
+    
+    
+}
